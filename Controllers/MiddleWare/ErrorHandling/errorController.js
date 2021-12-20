7aca98b468059e4251e82f85b8b7face8dfbe024
Brandon Mitchell <83912617+Sanda2399@users.noreply.github.com>
2021-12-20 09:24:19 -0500

    Updated Error Controller

--- Controllers/MiddleWare/ErrorHandling/errorController.js
+++ Controllers/MiddleWare/ErrorHandling/errorController.js
@@ -2,24 +2,30 @@ const AppError = require('../../../Utilities/appError');
 
 ///// ERROR HANDLER FUNCTIONS /////
 const handleCastErrorDB = err => {
-    const message = `Invaild ${err.path}: ${err.value}.`;
+    const message = `Invalid ${err.path}: ${err.value}.`;
     return new AppError(message, 400);
-}
+};
 
 const handleDuplicateFieldsDB = err => {
-    const value = err.keyValue.name || err.keyValue.email;
-    const message = `${value} is already in use. Please try another value.`;
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    console.log(value);
+
+    const message = `Duplicate field value: ${value}. Please use another value!`;
     return new AppError(message, 400);
-}
+};
 
 const handleValidationErrorDB = err => {
-    const errors = Object.values(err.errors).map( val => val.message )
+    const errors = Object.values(err.errors).map(el => el.message);
+
     const message = `Invalid input data. ${errors.join('. ')}`;
     return new AppError(message, 400);
-}
+};
 
-const handleJWTError = err => new AppError('Invalid token. Please login again.', 401);
-const handleJWTExpiredError = err => new AppError('Expired token. Please login again.', 401);
+const handleJWTError = () =>
+    new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = () =>
+    new AppError('Your token has expired! Please log in again.', 401);
 
 
 ///// Development Error Response /////
