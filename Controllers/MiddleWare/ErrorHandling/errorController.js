f7cd4c4ec8632483462391bc01569c4ff41dc1ac
Brandon Mitchell <83912617+Sanda2399@users.noreply.github.com>
2021-12-20 10:53:48 -0500

    Updated Error Controller

--- Controllers/MiddleWare/ErrorHandling/errorController.js
+++ Controllers/MiddleWare/ErrorHandling/errorController.js
@@ -55,25 +55,42 @@ const sendErrorDev = (err, req, res) => {
 
 ///// Production Error Response /////
 const sendErrorProd = (err, req, res) => {
-    const msg = err.isOperational ? err.message : 'this is unexpected -- please contact support';
-    
-    if(req.originalUrl.match(/^[/]api[/]v/)) 
-    // * Response to send if doing a api request *
-    {
-        return res.status(err.statusCode).json({
-            status: err.status,
-            message: msg
-        });        
-    } 
-    else 
-    // * Response to send if using the rendered website *
-    {
+    // A) API
+    if (req.originalUrl.startsWith('/api')) {
+        // A) Operational, trusted error: send message to client
+        if (err.isOperational) {
+            return res.status(err.statusCode).json({
+                status: err.status,
+                message: err.message
+            });
+        }
+        // B) Programming or other unknown error: don't leak error details
+        // 1) Log error
+        console.error('ERROR 💥', err);
+        // 2) Send generic message
+        return res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong!'
+        });
+    }
+
+    // B) RENDERED WEBSITE
+    // A) Operational, trusted error: send message to client
+    if (err.isOperational) {
         return res.status(err.statusCode).render('error', {
-            title: 'Something went wrong',
-            msg: msg
-        });        
-    }    
-}
+            title: 'Something went wrong!',
+            msg: err.message
+        });
+    }
+    // B) Programming or other unknown error: don't leak error details
+    // 1) Log error
+    console.error('ERROR 💥', err);
+    // 2) Send generic message
+    return res.status(err.statusCode).render('error', {
+        title: 'Something went wrong!',
+        msg: 'Please try again later.'
+    });
+};
 
 
 ///// MAIN /////
